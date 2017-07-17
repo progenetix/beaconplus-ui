7f8f048e291563625147dc5ebd4a8793647831d7
Michael Baudis <m@baud.is>
2017-07-17 15:14:35 +0200

    fixing response table

--- js/query.js
+++ js/query.js
@@ -32,19 +32,19 @@ $( "#beacon-form" ).submit(function( event ) {
                 $("#spinner").hide();
                 $("#noResult").hide();
                 $("#result").show();
-                
+
                 // this already implements the responses for multiple datasets
                 var dataset_no = data.dataset_allele_responses.length;
 				for (var i = 0; i < dataset_no; i++) {
 
-					var result = '<td>'+ data.dataset_allele_responses[i].dataset_id +'</td>';
+					var result = '';
 					$.each(formParam, function (i, val) {
 						result += '<td>'+ val.value +'</td>';
 					});
 					result += '<td>'+ data.dataset_allele_responses[i].call_count +'</td>';
 
 					$("#resultTable").append('<tr>' + result + '</tr>');
-					
+
                 }
             })
             .fail(function (jqXHR, textStatus, error) {
@@ -68,6 +68,7 @@ function buildQuery(params) {
 
     var query = '';
     var paramName2Url = {
+        "datasetId": "dataset_id",
         "referenceName": "variants.reference_name",
         "variantType": "variants.variant_type",
         "start1": "variants.start_max",
