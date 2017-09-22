e8eb8b45fc8c2ffb94c55f8549746e53d53abdf2
mbaudis <m@baud.is>
2017-09-22 13:52:14 +0200

    more docs

--- js/query.js
+++ js/query.js
@@ -68,8 +68,8 @@ $( "#beacon-form" ).submit(function( event ) {
     				// $.each(formParam, function (i, val) {
     				// 	result += '<td>' + val.value +'</td>';
     				// });
-            result += '<td>'+ data.dataset_allele_responses[i].variant_count +'<br/>' + data.dataset_allele_responses[i].call_count +'<br/>' +data.dataset_allele_responses[i].sample_count +'</td>';
-            result += '<td>'+ data.dataset_allele_responses[i].info.bs_match_frequency + '<br/>' + data.dataset_allele_responses[i].frequency +'</td>';
+            result += '<td>'+ data.dataset_allele_responses[i].variant_count +'<br/>' + data.dataset_allele_responses[i].call_count +'<br/>' + data.dataset_allele_responses[i].callset_count +'<br/>' + data.dataset_allele_responses[i].sample_count +'</td>';
+            result += '<td>' + data.dataset_allele_responses[i].frequency + '<br/>' + data.dataset_allele_responses[i].info.bs_match_frequency + '</td>';
             result += '<td><a href="' + ARRAYMAP + '/?' + data.info.query_string +'" title="' + data.info.query_string + '" target="_BLANK">JSON</a><br/><a href="http://www.genome.ucsc.edu/cgi-bin/hgTracks?db=' + ucscgenome + '&position=chr' + $("#referenceName").val() + '%3A' + ucscstart + '%2D' + ucscend + '" target="_blank">UCSC</a></td>';
 
     				$("#resultTable").append('<tr>' + result + '</tr>');
