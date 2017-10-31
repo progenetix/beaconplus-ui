4bf380a3d3cffe31ee5737bd9e12022bd8c8bf7a
mbaudis <m@baud.is>
2017-10-31 14:06:59 +0100

    adapting to schema conform response

--- js/query.js
+++ js/query.js
@@ -69,8 +69,8 @@ $( "#beacon-form" ).submit(function( event ) {
     				// $.each(formParam, function (i, val) {
     				// 	result += '<td>' + val.value +'</td>';
     				// });
-            result += '<td>'+ data.dataset_allele_responses[i].variant_count +'<br/>' + data.dataset_allele_responses[i].call_count +'<br/>' + data.dataset_allele_responses[i].callset_count +'<br/>' + data.dataset_allele_responses[i].sample_count +'</td>';
-            result += '<td>' + data.dataset_allele_responses[i].frequency + '<br/>' + data.dataset_allele_responses[i].info.bs_match_frequency + '</td>';
+            result += '<td>'+ data.dataset_allele_responses[i].variant_count +'<br/>' + data.dataset_allele_responses[i].call_count +'<br/>' + data.dataset_allele_responses[i].sample_count +'</td>';
+            result += '<td>' + data.dataset_allele_responses[i].frequency + '</td>';
             result += '<td><a href="' + ARRAYMAP + '/?' + data.info.query_string +'" title="' + data.info.query_string + '" target="_BLANK">JSON</a><br/><a href="http://www.genome.ucsc.edu/cgi-bin/hgTracks?db=' + ucscgenome + '&position=chr' + $("#referenceName").val() + '%3A' + ucscstart + '%2D' + ucscend + '" target="_blank">UCSC</a><br/><a href="' + HANDOVER + '/?accessid=' + data.dataset_allele_responses[i].info.callset_access_handle + '" title="Data Handover" target="_BLANK">Handover</a></td>';
 
     				$("#resultTable").append('<tr>' + result + '</tr>');
